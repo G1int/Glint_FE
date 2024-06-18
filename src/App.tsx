43d2cb0fe0c84057247422aef290eeda40574a79
zinnli <limgx5192@gmail.com>
2024-06-18 16:18:32 +0900

    feat:  router 코드 추가 및 page 임시 코드 추가 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "components";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Router />;
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
 export default App;
--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const Router = () => {
-  return <div>Router</div>;
+import { Home } from "pages";
+
+interface RouterProps {
+  children?: React.ReactNode; //TODO: Modal, Toast 컴포넌트 추가 이후 필수값으로 변경 필요
+}
+
+const Router = ({ children }: RouterProps) => {
+  return (
+    <BrowserRouter>
+      {children}
+      <Routes>
+        {/* NOTE: 임시 코드 */}
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
