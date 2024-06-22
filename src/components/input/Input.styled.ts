4dbb719baac0c3faaa9725214ab7515cd3498b58
zinnli <limgx5192@gmail.com>
2024-06-22 14:50:32 +0900

    feat: 공용 input 제작 (#7)

--- src/components/input/Input.styled.ts
+++ src/components/input/Input.styled.ts
@@ -1,16 +1,6 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const Input = styled.input`
-  border: 1px solid cadetblue;
-`;
-
-export const icon = css`
-  width: 16px;
-  height: 16px;
-  transform: rotate(90deg);
-
-  & > path {
-    fill: blue;
-  }
+  border: 0;
+  border-bottom: 1px solid black;
 `;
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 
 import * as S from "./Input.styled";
-import { ArrowIcon } from "assets";
 
-const Input = () => {
+interface InputProps {
+  //TODO: 기능 정의서 확인 후 type 필수값 수정 필요
+  className?: string;
+  type: "text";
+  value: string;
+  placeholder?: string;
+  maxLength?: number;
+  disabled?: boolean;
+  autoComplete?: "on" | "off";
+  handleBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  className,
+  type,
+  value,
+  placeholder,
+  maxLength,
+  disabled,
+  autoComplete,
+  handleBlur,
+  handleChange,
+}: InputProps) => {
   return (
     <>
-      <S.Input />
-      <ArrowIcon css={S.icon} />
+      <S.Input
+        className={className}
+        type={type}
+        value={value}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        disabled={disabled}
+        autoComplete={autoComplete}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
     </>
   );
 };
