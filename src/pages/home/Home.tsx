f53a35af884af541b0b81b1d212511877de3a53e
zinnli <limgx5192@gmail.com>
2024-06-22 14:31:35 +0900

    chore: 토스트 발생 임시 코드 제작 (#5)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-import { Input } from "components";
+import { useToast } from "hooks";
 
 const Home = () => {
+  const { addToast } = useToast();
+
+  const toastContext = {
+    content: "토스트 테스트중",
+  };
+
   return (
     <div>
       home
-      <Input />
+      <button onClick={() => addToast(toastContext)}>toast 테스트</button>
     </div>
   );
 };
