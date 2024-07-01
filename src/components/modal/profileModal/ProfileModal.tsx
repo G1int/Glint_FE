41681bbe73361196d50b9394fd108e9dd9a5be0e
dlskfo306 <dlskfo306@gmail.com>
2024-07-01 20:00:35 +0900

    feat: profileModal 제작-이미지, 텍스트 (#17)

--- src/components/modal/profileModal/ProfileModal.tsx
+++ src/components/modal/profileModal/ProfileModal.tsx
@@ -1,15 +1,66 @@
+import { Badge } from "components/badge";
 import { BaseModal } from "../baseModal";
 import * as S from "./ProfileModal.styled";
 
 interface ProfileModalProps {
-  content?: React.ReactNode;
   img?: string;
+  name?: string;
+  age?: string;
+  info?: { location: string; company: string; job: string };
+  introduceInfo?: {
+    introduce: string;
+    basicInfo: string[];
+    keywords: string[];
+  };
 }
 
-const ProfileModal = ({ content, img }: ProfileModalProps) => {
+const ProfileModal = ({
+  introduceInfo,
+  img,
+  name,
+  age,
+  info,
+}: ProfileModalProps) => {
   return (
     <BaseModal mode={"bottom"}>
-      <S.ModalBody>{img && <S.Img src={img} alt="profile img" />}</S.ModalBody>
+      <S.ModalBody mode={img ? "img" : "text"}>
+        {img && info && (
+          <>
+            <S.Img src={img} alt="profile img" />
+            <S.MainInfoContent>
+              <span>{name}</span>
+              <span>{age}세</span>
+            </S.MainInfoContent>
+            <S.InfoContent>
+              <span>{info.location}</span>
+              <span>{info.company}</span>
+              <span>{info.job}</span>
+            </S.InfoContent>
+          </>
+        )}
+        {introduceInfo && (
+          <>
+            <S.Title>자기소개</S.Title>
+            <S.IntroduceContent>{introduceInfo?.introduce}</S.IntroduceContent>
+            <S.Title>기본정보</S.Title>
+            <S.IntroduceContent>
+              {introduceInfo.basicInfo.map((item, index) => (
+                <Badge key={index} size="sm" disabled>
+                  {item}
+                </Badge>
+              ))}
+            </S.IntroduceContent>
+            <S.Title>나를 표현하는 키워드</S.Title>
+            <S.IntroduceContent>
+              {introduceInfo.keywords.map((item, index) => (
+                <Badge key={index} disabled>
+                  {item}
+                </Badge>
+              ))}
+            </S.IntroduceContent>
+          </>
+        )}
+      </S.ModalBody>
     </BaseModal>
   );
 };
