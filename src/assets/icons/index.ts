e45010372ca06b198c310900498a182f5b2672c2
zinnli <limgx5192@gmail.com>
2024-07-06 13:30:22 +0900

    chore: 뒤로가기 아이콘 추가 (#30)

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as ArrowIcon } from "./ic_arrow.svg";
 export { ReactComponent as CameraIcon } from "./ic_camera.svg";
+export { ReactComponent as ChevronLeftIcon } from "./ic_chevronLeft.svg";
 export { ReactComponent as HeartIcon } from "./ic_heart.svg";
