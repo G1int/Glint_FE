9a71b0b643641afe910a78289de4b62dbdec96e8
zinnli <limgx5192@gmail.com>
2024-07-06 21:30:42 +0900

    feat: 뒤로가기 레이아웃 props 추가 -> 기존 뒤로가기 외 회원가입처럼 받아서 처리해야하는 경우(#30)

--- src/components/layout/backLayout/BackLayout.tsx
+++ src/components/layout/backLayout/BackLayout.tsx
@@ -7,9 +7,14 @@ import * as S from "./BackLayout.styled";
 interface BackLayoutProps {
   className?: string;
   children: React.ReactNode;
+  handleClickBack?: () => void;
 }
 
-const BackLayout = ({ className, children }: BackLayoutProps) => {
+const BackLayout = ({
+  className,
+  children,
+  handleClickBack,
+}: BackLayoutProps) => {
   const navigate = useNavigate();
 
   const onClickBack = (): void => {
@@ -20,7 +25,7 @@ const BackLayout = ({ className, children }: BackLayoutProps) => {
     <S.BackLayout className={className}>
       <S.Header>
         {/* TODO: 헤더에 타이틀 들어가는 경우가 있어 스크린 나오면 수정 필요 */}
-        <button onClick={onClickBack}>
+        <button onClick={handleClickBack ?? onClickBack}>
           {/* TODO: button 컴포넌트는 컬러로 인해 사용이 불가능하여 임시로 button 사용 추후 수정 필요 */}
           <ChevronLeftIcon />
         </button>
