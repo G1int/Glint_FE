9faff2f9b9ba551b5b51369b9f46fdfda7b47a90
zinnli <limgx5192@gmail.com>
2024-07-06 22:04:08 +0900

    feat: 회원가입 성별 선택 스크린 및 기능 추가 (#30)

--- src/pages/signup/contatiners/signupGender/SignupGender.tsx
+++ src/pages/signup/contatiners/signupGender/SignupGender.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import type { UseFormRegister, UseFormWatch } from "react-hook-form";
 
-const SignupGender = () => {
-  return <div>SignupGender</div>;
+import { CheckIcon } from "assets";
+import type { SignupForm } from "types";
+import * as S from "../../Signup.styled";
+
+interface SignupGenderProps {
+  watch: UseFormWatch<SignupForm>;
+  register: UseFormRegister<SignupForm>;
+}
+
+const SignupGender = ({ watch, register }: SignupGenderProps) => {
+  return (
+    <>
+      <S.Title>
+        <strong>성별</strong>을<br />
+        알려주세요.
+      </S.Title>
+      <S.FormRadioButtonWrapper>
+        <S.FormRadioButton isSelected={watch("gender") === "male"}>
+          <input type="radio" value="male" {...register("gender")} />
+          남자
+          <CheckIcon css={S.checkIcon(watch("gender") === "male")} />
+        </S.FormRadioButton>
+        <S.FormRadioButton isSelected={watch("gender") === "female"}>
+          <input type="radio" value="female" {...register("gender")} />
+          여자
+          <CheckIcon css={S.checkIcon(watch("gender") === "female")} />
+        </S.FormRadioButton>
+      </S.FormRadioButtonWrapper>
+    </>
+  );
 };
 
 export default SignupGender;
