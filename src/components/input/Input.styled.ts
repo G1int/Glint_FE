0904f314870465162fdc59eabe18bf7dd93d7a9d
zinnli <limgx5192@gmail.com>
2024-07-06 21:29:22 +0900

    feat: formInput props 추가 및 css 수정 (#30)

--- src/components/input/Input.styled.ts
+++ src/components/input/Input.styled.ts
@@ -1,9 +1,40 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 
+export const InputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const Input = styled.input<{ hasError?: boolean }>`
-  ${({ hasError }) => css`
+  ${({ theme, hasError }) => css`
+    height: 34px;
     border: 0;
-    border-bottom: 1px solid ${hasError ? "red" : "black"};
+    border-bottom: 1px solid
+      ${hasError ? theme.colors.red100 : theme.colors.gray400};
+    padding: 2px 0 12px;
+    font-size: 16px;
+
+    &::placeholder {
+      color: ${theme.colors.gray200};
+    }
+  `}
+`;
+
+export const Label = styled.span<{ hasError?: boolean }>`
+  ${({ theme, hasError }) => css`
+    margin-bottom: 6px;
+    font-size: 14px;
+    color: ${hasError ? theme.colors.red100 : theme.colors.black};
+  `}
+`;
+
+export const Message = styled.span<{ hasError?: boolean }>`
+  ${({ theme, hasError }) => css`
+    display: flex;
+    margin-top: 8px;
+    color: ${hasError && theme.colors.red100};
+    font-size: 12px;
   `}
 `;
--- src/components/input/formInput/FormInput.tsx
+++ src/components/input/formInput/FormInput.tsx
@@ -7,12 +7,16 @@ interface FormInputProps {
   //TODO: 기능 정의서 확인 후 type 필수값 수정 필요
   className?: string;
   type?: "text";
+  label?: string;
   value?: string;
+  min?: number;
+  max?: number;
   placeholder?: string;
   maxLength?: number;
   disabled?: boolean;
   autoComplete?: "on" | "off";
   hasError?: boolean;
+  message?: string;
   handleFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
   register: UseFormRegisterReturn<string>;
 }
@@ -20,28 +24,38 @@ interface FormInputProps {
 const FormInput = ({
   className,
   type,
+  label,
   value,
+  min,
+  max,
   placeholder,
   maxLength,
   disabled,
   autoComplete,
   hasError,
+  message,
   handleFocus,
   register,
 }: FormInputProps) => {
   return (
-    <S.Input
-      className={className}
-      type={type}
-      value={value}
-      placeholder={placeholder}
-      maxLength={maxLength}
-      disabled={disabled}
-      autoComplete={autoComplete}
-      hasError={hasError}
-      onFocus={handleFocus}
-      {...register}
-    />
+    <S.InputWrapper>
+      {label && <S.Label hasError={hasError}>{label}</S.Label>}
+      <S.Input
+        className={className}
+        type={type}
+        value={value}
+        min={min}
+        max={max}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        disabled={disabled}
+        autoComplete={autoComplete}
+        hasError={hasError}
+        onFocus={handleFocus}
+        {...register}
+      />
+      {message && <S.Message hasError={hasError}>{message}</S.Message>}
+    </S.InputWrapper>
   );
 };
 
