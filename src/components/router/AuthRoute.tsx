60fd5d749511fb23d57d4d19095537074e6a22eb
dlskfo306 <dlskfo306@gmail.com>
2024-07-06 14:42:31 +0900

    feat: AuthRoute구현, Router 수정 (#27)

---
+++ src/components/router/AuthRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const AuthRoute = () => {
+  const accessToken = localStorage.getItem("accessToken");
+
+  return accessToken ? <Outlet /> : <Navigate to={"/"} />;
+};
+
+export default AuthRoute;
--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { Layout } from "components";
 import * as P from "pages";
+import AuthRoute from "./AuthRoute";
 
 interface RouterProps {
   children?: React.ReactNode; //TODO: Modal, Toast 컴포넌트 추가 이후 필수값으로 변경 필요
@@ -15,8 +16,11 @@ const Router = ({ children }: RouterProps) => {
       <Routes>
         {/* NOTE: 임시 코드 */}
         <Route element={<Layout />}>
-          <Route index element={<P.Home />} />
-          <Route path="/signup" element={<P.Signup />} />
+          <Route index path="/signup" element={<P.Signup />} />
+          <Route path="/glint/auth/kakao/callback" element={<P.Auth />} />
+          <Route element={<AuthRoute />}>
+            <Route path="/" element={<P.Home />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
