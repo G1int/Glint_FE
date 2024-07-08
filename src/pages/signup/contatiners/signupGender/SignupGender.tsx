1985e1e7980ec553df6fe13531efaca4d9107420
zinnli <102575747+zinnli@users.noreply.github.com>
2024-07-08 22:08:28 +0900

    feat: formRadioButton 컴포넌트 제작 (#34) (#42)

--- src/pages/signup/contatiners/signupGender/SignupGender.tsx
+++ src/pages/signup/contatiners/signupGender/SignupGender.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { UseFormRegister, UseFormWatch } from "react-hook-form";
 
-import { CheckIcon } from "assets";
+import { FormRadioButton } from "components";
 import type { SignupForm } from "types";
 import * as S from "../../Signup.styled";
 
@@ -17,18 +17,15 @@ const SignupGender = ({ watch, register }: SignupGenderProps) => {
         <strong>성별</strong>을<br />
         알려주세요.
       </S.Title>
-      <S.FormRadioButtonWrapper>
-        <S.FormRadioButton isSelected={watch("gender") === "male"}>
-          <input type="radio" value="male" {...register("gender")} />
-          남자
-          <CheckIcon css={S.checkIcon(watch("gender") === "male")} />
-        </S.FormRadioButton>
-        <S.FormRadioButton isSelected={watch("gender") === "female"}>
-          <input type="radio" value="female" {...register("gender")} />
-          여자
-          <CheckIcon css={S.checkIcon(watch("gender") === "female")} />
-        </S.FormRadioButton>
-      </S.FormRadioButtonWrapper>
+      <FormRadioButton
+        css={S.formRadioButton}
+        radioList={[
+          { key: "male", label: "남자" },
+          { key: "female", label: "여자" },
+        ]}
+        value={watch("gender") ?? ""}
+        register={register("gender")}
+      />
     </>
   );
 };
