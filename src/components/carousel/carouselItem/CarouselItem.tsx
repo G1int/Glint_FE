f95033b8c8a7605122ae7465ba10094095318af1
dlskfo306 <dlskfo306@gmail.com>
2024-07-08 17:00:33 +0900

    feat: 캐로셀 컴포넌트 수정 (#38)

--- src/components/carousel/carouselItem/CarouselItem.tsx
+++ src/components/carousel/carouselItem/CarouselItem.tsx
@@ -3,17 +3,17 @@ import React from "react";
 import * as S from "./CarouselItem.styled";
 
 interface CarouselItemProps {
-  title: string;
-  content: string;
+  title: React.ReactNode;
+  content: React.ReactNode;
   img: string;
 }
 
 const CarouselItem = ({ title, content, img }: CarouselItemProps) => {
   return (
-    <S.CarouselBox key={title}>
+    <S.CarouselBox>
       <S.ContentWrapper>
-        <span>{title}</span>
-        <span>{content}</span>
+        <S.Title>{title}</S.Title>
+        <S.Content>{content}</S.Content>
       </S.ContentWrapper>
       <S.Img src={img} />
     </S.CarouselBox>
