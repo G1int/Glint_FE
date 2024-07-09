3d1c36b5b1b3d5371cf2e06fd918bf4b7bc73bbd
zinnli <limgx5192@gmail.com>
2024-07-09 23:54:40 +0900

    style: font 디자인 시스템 정의 (#47)

--- src/styles/themes/fonts.ts
+++ src/styles/themes/fonts.ts
@@ -1,3 +1,86 @@
-export const fonts = {} as const;
+import { css } from "@emotion/react";
+
+export const fonts = {
+  title_bold_40: css`
+    font-size: 4rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  subCopy_bold_32: css`
+    font-size: 3.2rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  subCopy_bold_28: css`
+    font-size: 2.8rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  subCopy_bold_24: css`
+    font-size: 2.4rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  subTitle_bold_16: css`
+    font-size: 1.6rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  subTitle_bold_14: css`
+    font-size: 1.4rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  caption_bold_12: css`
+    font-size: 1.2rem;
+    font-weight: 700;
+    font-family: "SUIT", sans-serif;
+  `,
+  headline_semibold_18: css`
+    font-size: 1.8rem;
+    font-weight: 600;
+    font-family: "SUIT", sans-serif;
+  `,
+  subTitle_semibold_16: css`
+    font-size: 1.6rem;
+    font-weight: 600;
+    font-family: "SUIT", sans-serif;
+  `,
+  subTitle_semibold_14: css`
+    font-size: 1.4rem;
+    font-weight: 600;
+    font-family: "SUIT", sans-serif;
+  `,
+  caption_semibold_12: css`
+    font-size: 1.2rem;
+    font-weight: 600;
+    font-family: "SUIT", sans-serif;
+  `,
+  caption_regular_16: css`
+    font-size: 1.6rem;
+    font-weight: 400;
+    font-family: "SUIT", sans-serif;
+  `,
+  caption_regular_14: css`
+    font-size: 1.4rem;
+    font-weight: 400;
+    font-family: "SUIT", sans-serif;
+  `,
+  caption_regular_12: css`
+    font-size: 1.2rem;
+    font-weight: 400;
+    font-family: "SUIT", sans-serif;
+  `,
+  subCopy_light_32: css`
+    font-size: 3.2rem;
+    font-weight: 300;
+    font-family: "SUIT", sans-serif;
+  `,
+  subCopy_light_28: css`
+    font-size: 2.8rem;
+    font-weight: 300;
+    font-family: "SUIT", sans-serif;
+  `,
+} as const;
 
 export type Fonts = typeof fonts;
