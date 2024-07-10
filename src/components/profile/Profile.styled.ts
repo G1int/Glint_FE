4205ae5667c23161f43c6f0d526801cc406cb282
dlskfo306 <dlskfo306@gmail.com>
2024-07-10 23:07:31 +0900

    style: 프로필 컴포넌트 수정 (#51)

--- src/components/profile/Profile.styled.ts
+++ src/components/profile/Profile.styled.ts
@@ -3,10 +3,38 @@ import styled from "@emotion/styled";
 
 export const Profile = styled.div`
   display: flex;
-  column-gap: 10px;
+  column-gap: 29px;
 
   span {
     font-size: 12px; //TODO: 디자인 나오면 수정 필요
+  }
+`;
+export const ImgContent = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+export const Img = styled.img`
+  width: 72px;
+  height: 72px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const IconContent = styled.button`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 30px;
+  border-radius: 50%;
+
+  svg {
+    width: 32px;
+    height: 32px;
   }
 `;
 
@@ -14,72 +42,30 @@ export const InfoWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  row-gap: 10px;
-`;
-
-export const Img = styled.img`
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  object-fit: cover;
-`;
-
-export const ImgContent = styled.div`
-  position: relative;
-  display: flex;
-`;
-
-export const IconContent = styled.button<{
-  isChangeProfile?: boolean;
-  isRoomManager?: boolean;
-}>`
-  ${({ isChangeProfile, isRoomManager }) => css`
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background-color: ${isChangeProfile ? "white" : isRoomManager && "yellow"};
-
-    svg {
-      width: 16px; //TODO: 디자인 나오면 수정 필요
-      height: 16px;
-    }
-  `}
-`;
-
-export const KeywordContent = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  column-gap: 5px;
-
-  & > span::before {
-    content: "#";
-  }
+  row-gap: 1px;
 `;
 
 export const MainInfoContent = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: flex-end;
-  column-gap: 5px;
+  font-weight: 500;
 `;
 
 export const Name = styled.span`
-  font-size: 18px !important;
+  font-size: 2rem !important;
 `;
 
 export const InfoContent = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  column-gap: 5px;
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: flex-start;
+    column-gap: 4px;
+    color: ${theme.colors.gray700};
 
-  & > span:not(:last-of-type) {
-    border-right: 1px solid black;
-    padding-right: 5px;
-  }
+    & > span:not(:last-of-type) {
+      border-right: 1px solid ${theme.colors.gray700};
+      padding-right: 4px;
+    }
+  `}
 `;
