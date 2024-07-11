bc89828507c966446ed84535d938f50a575fa273
dlskfo306 <dlskfo306@gmail.com>
2024-07-11 23:01:11 +0900

    feat: footer 클릭 부분 path로 수정 (#51)

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,34 +1,37 @@
 import { Button } from "components/buttons";
 import * as S from "./Footer.styled";
 import { Home, MyInfo, MyMeeting, Search } from "assets";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Footer = () => {
-  const [isSelected, setIsSelected] = useState("홈");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  const handleClickFooter = (text: string) => {
-    setIsSelected(text);
+  const handleClickFooter = (path: string) => {
+    navigate(path);
   };
 
+  // TODO: path 임의지정
   const footerArr = [
-    { icon: <Home />, text: "홈" },
-    { icon: <Search />, text: "검색" },
+    { icon: <Home />, text: "홈", path: "/home" },
+    { icon: <Search />, text: "검색", path: "/search" },
     {
       icon: <MyMeeting />,
       text: "내미팅",
+      path: "/myMeeting",
     },
-    { icon: <MyInfo />, text: "내정보" },
+    { icon: <MyInfo />, text: "내정보", path: "/myInfo" },
   ];
 
   return (
     <S.Footer>
       {footerArr.map((item) => {
-        const selectedItem = isSelected === item.text;
+        const selectedItem = item.path === location.pathname;
         return (
           <Button
             css={S.button}
             key={item.text}
-            onClick={() => handleClickFooter(item.text)}
+            onClick={() => handleClickFooter(item.path)}
           >
             <S.ButtonWrapper>
               <S.IconWrapper isSelected={selectedItem} text={item.text}>
