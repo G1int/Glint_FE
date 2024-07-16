9de6f45ac1f34f62a6f484ec54fb5e68ee20c363
zinnli <limgx5192@gmail.com>
2024-07-16 23:24:30 +0900

    feat: 미팅 홈 레이아웃 작성 (#63)

--- src/pages/meeting/Meeting.tsx
+++ src/pages/meeting/Meeting.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-import { BackLayout } from "components";
+import { BackLayout, Tab } from "components";
+import { Application, Chatting, Home } from "./containers";
 
 const Meeting = () => {
+  const isOwner = false;
+
+  const tabs = {
+    홈: <Home />,
+    채팅: <Chatting />,
+    신청목록: <Application />,
+  };
+
   return (
-    <BackLayout isMeeting isOwner title="미팅방만들기">
-      미팅
+    <BackLayout isMeeting isOwner>
+      <Tab
+        tabList={isOwner ? ["홈", "채팅", "신청목록"] : ["홈", "채팅"]}
+        tabs={tabs}
+      />
     </BackLayout>
   );
 };
