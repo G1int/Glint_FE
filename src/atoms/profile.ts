56eb5f7129c8ce36c0fbb7f939bfdceffb60753d
dlskfo306 <dlskfo306@gmail.com>
2024-07-22 19:04:17 +0900

    feat: userId, userDetail, userProfile 전역관리 (#83)

--- src/atoms/profile.ts
+++ src/atoms/profile.ts
@@ -1,8 +1,10 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { userDetailType, userProfileType } from "types";
 
 export const profileState = atom({
   key: "profileState",
   default: {
+    profileImageUrl: "",
     workName: "",
     universityName: "",
     universityDepartment: "",
@@ -14,4 +16,20 @@ export const profileState = atom({
     selfIntroduction: "",
     hashtags: [],
   },
+});
+
+export const userIdSelector = selector<string | null>({
+  key: "userIdSelector",
+  get: () => {
+    return sessionStorage.getItem("id");
+  },
+});
+export const userDetailState = atom<userDetailType | null>({
+  key: "userDetailState",
+  default: null,
+});
+
+export const userProfileState = atom<userProfileType | null>({
+  key: "userProfileState",
+  default: null,
 });
---
+++ src/hooks/useUserInfo.ts
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { getUserInfoAPI } from "apis";
+import { userDetailState, userIdSelector, userProfileState } from "atoms";
+
+const useUserInfo = () => {
+  const userId = useRecoilValue(userIdSelector);
+  const [userDetail, setUserDetail] = useRecoilState(userDetailState);
+  const [userProfile, setUserProfile] = useRecoilState(userProfileState);
+
+  const fetchUserInfo = async () => {
+    if (userId) {
+      try {
+        const data = await getUserInfoAPI(userId);
+        setUserDetail(data.userDetail);
+        setUserProfile(data.userProfile);
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserInfo();
+  }, [userId]);
+
+  return { userDetail, userProfile, fetchUserInfo };
+};
+
+export default useUserInfo;
