a7f59c1364fa3b44a7794d0c668eb86a62b072eb
dlskfo306 <dlskfo306@gmail.com>
2024-07-24 23:08:43 +0900

    feat: 참가미팅 구현 (#104)

--- src/pages/myMeeting/containers/joinMeeting/JoinMeeting.tsx
+++ src/pages/myMeeting/containers/joinMeeting/JoinMeeting.tsx
@@ -1,26 +1,45 @@
-import Img from "assets/images/img_01.jpg";
-import { MeetingCard } from "components";
-import { useState } from "react";
 import * as S from "./JoinMeeting.styled";
+import { useRecoilValue } from "recoil";
+import { userIdSelector } from "atoms";
+import { useGetMyMeeting } from "services";
+import { useEffect, useState } from "react";
+import { meetingListItem } from "types";
+import { MeetingCard } from "components";
+import { MoreIcon } from "assets";
 
 const JoinMeeting = () => {
-  // TODO: 예시코드-api연동 후 삭제
-  const [meetings, setMeetings] = useState<Array<{ id: number; img: string }>>([
-    { id: 1, img: Img },
-    { id: 2, img: Img },
-    { id: 3, img: Img },
-    { id: 4, img: Img },
-    { id: 5, img: Img },
-    { id: 6, img: Img },
-    { id: 7, img: Img },
-    { id: 8, img: Img },
-    { id: 9, img: Img },
-    { id: 10, img: Img },
-  ]);
+  const [lastMeetingId, setLastMeetingId] = useState<number | null>(null);
+  const [meetingList, setMeetingList] = useState<meetingListItem[]>([]);
+
+  const userId = useRecoilValue(userIdSelector);
+
+  const limit = 3;
+
+  const { data } = useGetMyMeeting("ACCEPTED", userId!, lastMeetingId, limit);
+
+  useEffect(() => {
+    if (data?.meetings) {
+      setMeetingList((prevMeetings) => [...prevMeetings, ...data.meetings]);
+    }
+  }, [data]);
+
+  const handleMoreMeeting = () => {
+    if (data?.meetings && data.meetings.length > 0) {
+      setLastMeetingId(data.meetings[data.meetings.length - 1].meetingId);
+    }
+  };
 
   return (
     <S.MeetingContainer>
-      <MeetingCard count={3} meetingList={meetings} />
+      <MeetingCard meetingList={meetingList} />
+      {meetingList.length >= limit &&
+        data &&
+        data?.meetings.length >= limit && (
+          <S.More onClick={handleMoreMeeting}>
+            더보기
+            <MoreIcon />
+          </S.More>
+        )}
     </S.MeetingContainer>
   );
 };
