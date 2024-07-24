19f3dfe0ce982b52af89b0fddf7caf64ae3a17f2
dlskfo306 <dlskfo306@gmail.com>
2024-07-24 23:04:08 +0900

    chore: 내미팅 tabs 컴포넌트 수정 (#104)

--- src/pages/myMeeting/MyMeeting.tsx
+++ src/pages/myMeeting/MyMeeting.tsx
@@ -4,9 +4,10 @@ import { JoinMeeting, WaitMeeting } from "./containers";
 
 const MyMeeting = () => {
   const tabs = [
-    { label: "참가 미팅", query: "accept", component: <WaitMeeting /> },
-    { label: "대기 미팅", query: "waiting", component: <JoinMeeting /> },
+    { label: "참가 미팅", query: "accepted", component: <JoinMeeting /> },
+    { label: "대기 미팅", query: "waiting", component: <WaitMeeting /> },
   ];
+
   return (
     <>
       <Header css={S.header}>내미팅</Header>
