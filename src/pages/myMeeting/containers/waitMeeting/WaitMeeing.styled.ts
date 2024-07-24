a67f0f8014b247159c6766902f9ed4584e543e7b
dlskfo306 <dlskfo306@gmail.com>
2024-07-24 23:08:58 +0900

    feat: 대기미팅 구현 (#104)

--- src/pages/myMeeting/containers/waitMeeting/WaitMeeing.styled.ts
+++ src/pages/myMeeting/containers/waitMeeting/WaitMeeing.styled.ts
@@ -1,7 +1,19 @@
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
 export const MeetingContainer = styled.div`
   flex: 1;
   overflow: auto;
   padding-bottom: 64px;
+`;
+
+export const More = styled.div`
+  ${({ theme }) => css`
+    ${theme.fonts.subTitle_semibold_16};
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  `}
 `;
