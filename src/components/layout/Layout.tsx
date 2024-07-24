82c200bb4892c624018468aa86af41e9c0b0e8ec
zinnli <limgx5192@gmail.com>
2024-07-24 21:46:35 +0900

    feat: footer 조건문 동적으로 받을 수 있도록 수정 (#95)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,19 +4,22 @@ import { Outlet, useLocation } from "react-router-dom";
 import * as S from "./Layout.styled";
 import { Footer } from "./footer";
 
+// TODO: 로그인, 회원가입, 채팅, Auth - Footer X
+const hideFooterPathPatterns = [
+  /^\/$/,
+  /^\/signup$/,
+  /^\/meeting$/,
+  /^\/myProfile$/,
+  /^\/glint\/auth\/kakao\/callback$/,
+  /^\/createRoom$/,
+  /^\/meeting\/\d+$/, // 예시: /meeting/123 처럼 숫자로 된 ID를 포함한 경로
+];
 const Layout = () => {
   const location = useLocation();
-  // TODO: 로그인, 회원가입, 채팅, Auth - Footer X
-  const hideFooterPath = [
-    "/",
-    "/signup",
-    "/meeting",
-    "/myProfile",
-    "/glint/auth/kakao/callback",
-    "/createRoom",
-  ];
 
-  const shouldHideFooter = hideFooterPath.includes(location.pathname);
+  const shouldHideFooter = hideFooterPathPatterns.some((pattern) =>
+    pattern.test(location.pathname)
+  );
 
   return (
     <S.Layout>
