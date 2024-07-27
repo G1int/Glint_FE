34b6252727144803e31c4bf53b4565ab82c335f4
dlskfo306 <dlskfo306@gmail.com>
2024-07-27 14:48:49 +0900

    style: Toast컴포넌트 css 수정 (#110)

--- src/components/toast/Toast.styled.ts
+++ src/components/toast/Toast.styled.ts
@@ -3,14 +3,18 @@ import styled from "@emotion/styled";
 export const Toast = styled.div`
   position: fixed;
   left: 50%;
-  bottom: 0px;
+  bottom: 70px;
   height: max-content;
   min-height: 52px;
   pointer-events: none;
   transform: translateX(-50%);
   z-index: 100;
+`;
 
-  & > div:not(:first-of-type) {
-    margin-top: 50px;
-  }
+export const ToastItemWrapper = styled.div<{ index: number }>`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: ${({ index }) => `calc(70px + ${index} * (52px + 14.5px))`};
+  pointer-events: none;
 `;
