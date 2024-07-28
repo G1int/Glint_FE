c49e221fc48d72f309d1abf5c1ea390c97c66f90
dlskfo306 <dlskfo306@gmail.com>
2024-07-28 14:12:33 +0900

    feat: 회원가입 여부 수정 - kakao callback res에 isCompleteDetail여부로 navigate (#123)

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,21 +1,18 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useGetKakaoInfo, useGetUser } from "services/user";
+import { useGetKakaoInfo } from "services/user";
 import * as P from "pages";
 
 const Auth = () => {
   const navigate = useNavigate();
   const params = new URL(document.location.toString()).searchParams;
   const code = params.get("code");
-  const [sessionId, setSessionId] = useState("");
-  const { data: kakaoData, error: kakaoError } = useGetKakaoInfo(code || "");
-  const { data: user, error: userError } = useGetUser(sessionId || "");
+  const { data: kakaoData, error: kakaoError } = useGetKakaoInfo(code!);
 
   useEffect(() => {
     if (kakaoData) {
       sessionStorage.setItem("id", JSON.stringify(kakaoData.id));
       sessionStorage.setItem("email", JSON.stringify(kakaoData.email));
-      setSessionId(kakaoData.id.toString());
     }
 
     if (kakaoError) {
@@ -24,28 +21,15 @@ const Auth = () => {
     }
   }, [kakaoData, kakaoError]);
 
-  // storage 이벤트 리스너를 등록하여 sessionId를 업데이트
   useEffect(() => {
-    const handleStorageChange = (event: StorageEvent) => {
-      if (event.key === "id") {
-        setSessionId(event.newValue || "");
+    if (kakaoData) {
+      if (kakaoData.isCompleteDetail) {
+        navigate("/main");
+      } else if (!kakaoData.isCompleteDetail) {
+        navigate("/signup");
       }
-    };
-
-    window.addEventListener("storage", handleStorageChange);
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (user?.role) {
-      navigate("/main");
     }
-    if (userError) {
-      navigate("/signup");
-    }
-  }, [sessionId, user, userError]);
+  }, [kakaoData]);
 
   return <P.Loading />;
 };
