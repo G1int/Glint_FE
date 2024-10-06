2f3dc1b0be097d8f5e70f549f74f1a2d123cb2b8
dlskfo306 <dlskfo306@gmail.com>
2024-10-06 18:51:43 +0900

    feat: storybook svg 설정 (#158)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react";
-import svgr from "@svgr/rollup";
+import svgr from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 // NOTE: src 경로 설정을 위해 추가
 // (https://velog.io/@otterji/Vite-typescript-%ED%99%98%EA%B2%BD%EC%97%90%EC%84%9C-path-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0 참고)
@@ -10,7 +10,15 @@ import checker from "vite-plugin-checker";
 export default defineConfig({
   plugins: [
     react(),
-    svgr(),
+    svgr({
+      svgrOptions: {
+        exportType: "named",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
     viteTsconfigPaths(),
     checker({
       typescript: true,
